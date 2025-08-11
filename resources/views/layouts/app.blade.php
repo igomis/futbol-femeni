482eacfa4f5e7ff5ffb2151d86d7022a03d92125
igomis <igomis@cipfpbatoi.es>
2025-08-11 12:40:11 +0200

    devcontainer.json

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>@yield('title','Guia de futbol femení')</title>
-  @vite(['resources/css/app.css','resources/css/equips.css'])
+  @vite(['resources/css/app.css' ])
 </head>
 <body class="font-sans bg-gray-100 text-gray-900">
   <header class="bg-blue-800 text-white p-4">
