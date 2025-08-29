a008eac2ebe0f6ba8ff299a07aefd12b41ba6d02
igomis <igomis@cipfpbatoi.es>
2025-08-29 13:26:23 +0200

    Service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\BaseRepository;
+use App\Repositories\EquipRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(BaseRepository::class,  EquipRepository::class);
+
     }
 
     /**
---
+++ app/Services/EquipService.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Services;
+
+use App\Repositories\EquipRepository;
+
+class EquipService {
+    public function __construct(private EquipRepository $repo) {}
+
+    public function llistar() {
+        return $this->repo->getAll();
+    }
+
+    public function guardar(array $data) {
+        return $this->repo->create($data);
+    }
+
+    public function actualitzar($id, array $data) {
+        return $this->repo->update($id, $data);
+    }
+
+    public function eliminar($id) {
+        return $this->repo->delete($id);
+    }
+}
+
